16d50a72cc19c45c321b3af468c48eee460757db
Stephen Leander <stephen.leander1@gmail.com>
2022-06-04 22:22:17 +0200

    Import Card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Scoreboard from './components/Scoreboard';
 import Display from './components/Display';
+import Card from './components/Card';
 import './App.css';
 
 const App = () => {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
-  const [cards, setCards] = useState([1, 2, 3, 4, 5]);
+  const [cards, setCards] = useState([]);
 
   return (
     <div className="App">
       <Scoreboard score={score} highScore={highScore}></Scoreboard>
-      <Display cards={cards}></Display>
+      <Display cards={cards} score={score}></Display>
     </div>
   );
 };
